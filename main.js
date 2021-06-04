937f506b64e227fdceb0035f54d4c855b33a8ebd
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-04 09:42:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-Yapılıyor... 
+const { GiveawaysManager } = require("discord-giveaways");
+const manager = new GiveawaysManager(client, {
+  storage: "./çekilişler.json",
+  updateCountdownEvery: 10000,
+  default: {
+    botsCanWin: false,
+    embedColor: "RANDOM",
+    reaction: "🎉"
+  }
+});
+
+client.giveawaysManager = manager;
