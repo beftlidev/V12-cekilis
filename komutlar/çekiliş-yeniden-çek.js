36404f74f35cfd303bba340871dcf7e6dec04449
iUgur <77486662+iUgur@users.noreply.github.com>
2021-06-04 09:05:17 +0300

    Create çekiliş-yeniden-çek.js

---
+++ komutlar/çekiliş-yeniden-çek.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+const path = require("path");
+
+module.exports.run = async (client, message, args) => {
+  
+  let hasPerm = message.member.hasPermission("MANAGE_MESSAGES");
+  let hasRole = message.member.roles.cache.find(r => r.name === "Giveaways");
+
+    if (hasPerm === false || !hasRole == null)
+      return message.channel.send(
+        new Discord.MessageEmbed()
+          .setTitle("**Hata**")
+          .setColor("RED")
+          .setDescription(
+            "Bu komutu kullanmak için `MANAGE_MESSAGES` yetkisine ihtiyacınız var."
+          )
+          .setTimestamp()
+      );
+
+    if (!args[0]) {
+      return message.channel.send(
+        new Discord.MessageEmbed()
+          .setTitle("**Hata**")
+          .setColor("RED")
+          .setDescription(
+            "Lütfen, yeniden çekmek istediğiniz çekiliş ID sini giriniz \n Örnek (g.yeniden-çek (çekiliş mesajı nın ıd si))"
+          )
+          .setTimestamp()
+      );
+    }
+        client.giveawaysManager.reroll(args[0], {
+      messages: {
+        congrat: "🎁 Tebrikler: {winners}"
+      }
+    });
+};
+
+module.exports.help = {
+  name: "yeniden-çek"
+};
